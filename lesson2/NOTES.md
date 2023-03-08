fde231052331f98fea5b0d8f7687786f019232c2
zadubanm02 <zadubanm02@gmail.com>
2023-03-08 21:29:55 +0100

    Lesson3

--- lesson2/NOTES.md
+++ lesson2/NOTES.md
@@ -2,3 +2,7 @@
 
 if i want to save some gas dont create public views
 just make the state variable public and its infering views function automatically
+
+#### Storage, Memory
+
+memory is stored during the scope of the function and its cheaper than storage
--- lesson2/contracts/Score.sol
+++ lesson2/contracts/Score.sol
@@ -2,17 +2,27 @@
 pragma solidity ^0.8.9;
 
 contract Score {
+    // all the state variables can be find 
+    // dont expose confidentional information
     uint256 public score;
     address owner;
 
+    // events can work like return statements 
+    // if u want to inform caller about something
+    // events are only visible outside of the contract
+    event ScoreSet(uint256);
+
+    // its like a map in javascript
+    mapping (address => uint256) scoreTable;
+
     modifier onlyOwner {
-        if(msg.sender == owner) {
-            _;
-        }
+        require(msg.sender == owner, "Not allowed");
+        _;
     }
 
     constructor() {
         score = 5;
+        emit ScoreSet(5);
         owner = msg.sender;
     }
 
@@ -23,5 +33,14 @@ contract Score {
 
      function setScore(uint256 _newScore) public onlyOwner{
         score = _newScore;
+        emit ScoreSet(_newScore);
+     }
+
+     function setPlayerScore(address player, uint256 value) public {
+        scoreTable[player] = value;
+     }
+
+     function getPlayersScore(address player) public view returns (uint256) {
+        return scoreTable[player];
      }
 }

b62924ce752514c488bcb977c662fc28f734b724
zadubanm02 <zadubanm02@gmail.com>
2023-03-16 20:16:58 +0100

    Badger NFT

---
+++ lesson2/contracts/BadgerNFT.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+
+contract BadgerNFT is ERC721, Pausable, Ownable, ERC721Burnable {
+    constructor() ERC721("BadgerNFT", "BNFT") {}
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+}
